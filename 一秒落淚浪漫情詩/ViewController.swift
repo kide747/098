a74f04e9e78366eba0db1700accb42823c1839cf
程謙 <b01105006@ntu.edu.tw>
2017-03-24 10:43:52 +0800

    Initial Commit

---
+++ 一秒落淚浪漫情詩/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  一秒落淚浪漫情詩
+//
+//  Created by 程謙 on 2017/3/24.
+//  Copyright © 2017年 程謙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
